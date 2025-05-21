7cb690d42e3d1cf7db6c65f3ba2a207348a5f30d
DawidEales <dawideales@gmail.com>
2025-05-21 14:22:25 +0200

    dont worry its a test

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 header("Location: http://localhost/teapots/frontend/pagebuilder.php?page=login");
 // will move to what is wheateley configure on your own 
 exit(); // Always exit after a redirect
-
+//new coment stuf
 ?>
